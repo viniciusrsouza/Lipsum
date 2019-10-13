477abd5405258fd3897d0632861d32e6e71ab21b
Mateus Lins <41246021+mateuslins@users.noreply.github.com>
2019-10-13 17:50:20 -0300

    layout do login finalizado

--- TAGclient/portal-tag/src/login/Login.js
+++ TAGclient/portal-tag/src/login/Login.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import './Login.css'
-import LoginCard from './LoginCard'
-import sha1 from 'crypto-js/sha1'
-import {authenticate} from '../utils'
+import React from "react";
+import "./Login.css";
+import LoginCard from "./LoginCard";
+import sha1 from "crypto-js/sha1";
+import { authenticate } from "../utils";
 
 class Login extends React.Component {
   render() {
     return (
-      <div>
+      <div class="log">
+        <button class="btn btn-outline-primary" onClick={this.goBack}>
+          Voltar para o site
+        </button>
         <LoginCard onClick={this.onLogin} />
       </div>
     );
   }
 
-    onLogin(){
-        const emailElement = document.getElementById('email')
-        const passwordElement = document.getElementById('password')
-        const email = emailElement.value
-        const password = sha1(passwordElement.value).toString()
-        authenticate(sha1(email + password), ()=>{})
-        window.location.href='/'
-    }
+  onLogin() {
+    const emailElement = document.getElementById("email");
+    const passwordElement = document.getElementById("password");
+    const email = emailElement.value;
+    const password = sha1(passwordElement.value).toString();
+    authenticate(sha1(email + password), () => {});
+    window.location.href = "/";
+  }
+
+  goBack() {
+    window.location.href = "/";
+  }
 }
 
 export default Login;
--- TAGclient/portal-tag/src/login/LoginCard.js
+++ TAGclient/portal-tag/src/login/LoginCard.js
@@ -1,11 +1,58 @@
-import React from 'react'
-import './LoginCard.css'
-import image from '../images/icon_user.png'
+import React from "react";
+import "./LoginCard.css";
+import image from "../images/icon_user.png";
 
-class LoginCard extends React.Component{
-    render(){
-        return(
-            <div class="wrapper">
+class LoginCard extends React.Component {
+  render() {
+    return (
+      <div class="text-center">
+        <form class="form-signin">
+          <img class="mb-4" src={image} alt="" width="72" height="72"></img>
+          <h1 class="h3 mb-3 font-weigth-normal">
+            Por favor, preencha os campos abaixo
+          </h1>
+          <label for="inputEmail" class="sr-only">
+            Email address
+          </label>
+          <input
+            type="email"
+            id="email"
+            class="form-control"
+            placeholder="Email"
+            required
+            autofocus
+          ></input>
+          <label for="inputPassword" class="sr-only">
+            Password
+          </label>
+          <input
+            for="inputPassword"
+            id="password"
+            type="password"
+            class="form-control"
+            placeholder="Senha"
+            required
+          ></input>
+          <p class="mt-3 mb-3 text-muted">
+            <a href="#">Esqueci minha senha</a>
+          </p>
+          <button
+            class="btn btn-lg btn-primary btn-block"
+            type="button"
+            value="Log in"
+            onClick={this.props.onClick}
+          >
+            Logar
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+export default LoginCard;
+
+{
+  /* <div class="wrapper">
                 <div id="formContent">
                     <div class="first">
                     <img src={image} id="icon" alt="User Icon" />
@@ -19,8 +66,5 @@ class LoginCard extends React.Component{
                     <a class="underlineHover" href="#">Forgot Password?</a>
                     </div>
                 </div>
-            </div>
-        )
-    }
+            </div> */
 }
-export default LoginCard
