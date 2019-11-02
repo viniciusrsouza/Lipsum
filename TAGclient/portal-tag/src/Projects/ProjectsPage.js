8a44928e2e480c0d07408d71f903d0430ef9bed8
Vinicius Souza <vinicius.rsouza07@gmail.com>
2019-11-02 19:45:25 -0300

    projetos carregando dinamicamente

--- TAGclient/portal-tag/src/Projects/ProjectsPage.js
+++ TAGclient/portal-tag/src/Projects/ProjectsPage.js
@@ -1,49 +1,46 @@
 import React, { Component } from "react";
 import NavBar from "../components/NavBar";
+import ReactDOM from 'react-dom'
 import "./ProjectsPage.css";
+import { get, endpoints } from "../network/http-methods";
 
 class ProjectsPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      projects: [
-        {
-          id: 2,
-          title: "second",
-          author: "fulano",
-          category: "categoria 1",
-          date: "29/10/2019"
-        },
-        {
-          id: 1,
-          title: "first",
-          author: "beltrano",
-          category: "categoria 2",
-          date: "20/10/2019"
-        }
-      ]
-    };
+    console.log("teste")
+    console.log(ReactDOM)
+    get(endpoints.projects, {}, response => {
+      console.log(response)
+      this.setState({
+        projects: response
+      })
+      const node = ReactDOM.
+      document.getElementById('projects').appendChild(
+      )
+    })
   }
 
   renderTableData() {
-    return this.state.projects.map((project, index) => {
-      const { id, title, author, category, date } = project;
-      return (
-        <tr>
-          <td>{id}</td>
-          <td>
-            <a href="/PageProject">{title}</a>
-          </td>
-          <td>{author}</td>
-          <td>{category}</td>
-          <td>{date}</td>
-        </tr>
-      );
-    });
+    if(this.state){
+      return this.state.projects.map((project, index) => {
+        const { id, titulo, categoria, data_publicacao } = project;
+        return (
+          <tr>
+            <td>{id}</td>
+            <td>
+              <a href="/PageProject">{titulo}</a>
+            </td>
+            <td>{"author"}</td>
+            <td>{categoria}</td>
+            <td>{data_publicacao}</td>
+          </tr>
+        );
+      });
+    }
   }
 
   renderTableHeader() {
-    let header = Object.keys(this.state.projects[0]);
+    const header = ['id', 'titulo', 'categoria', 'data_publicacao']
     return header.map((key, index) => {
       return (
         <th scope="col" key={index}>
@@ -69,12 +66,14 @@ class ProjectsPage extends Component {
               Publicar Projeto
             </a>
           </div>
-          <table class="table table-striped">
+          <div id="projects">
+          <table class="table table-striped" id="projects">  
             <thead>
               <tr>{this.renderTableHeader()}</tr>
             </thead>
             <tbody>{this.renderTableData()}</tbody>
           </table>
+          </div>
         </div>
       </div>
     );
